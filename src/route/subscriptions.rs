f8c1ae9f54a8fd2da0e5de0513e2ea0a429e869a
Aminu 'Seun Joshua <34725212+seun-ja@users.noreply.github.com>
2022-11-29 15:43:35 +0100

    db name changed

--- src/route/subscriptions.rs
+++ src/route/subscriptions.rs
@@ -5,6 +5,7 @@ use actix_web::{
 use serde::Deserialize;
 use sqlx::PgPool;
 use uuid::Uuid;
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Deserialize)]
 pub struct FormData {
@@ -21,10 +22,27 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
+    
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
+}
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s
+        .chars()
+        .any(|g| forbidden_characters.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters) 
 }
 
 #[tracing::instrument(
