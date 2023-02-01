c2cc65c58c4f7df86b1dd56307f45b483658f043
Aminu 'Seun Joshua <34725212+seun-ja@users.noreply.github.com>
2023-02-01 12:22:42 +0100

    fixes configurations, including redis

--- src/configuration.rs
+++ src/configuration.rs
@@ -65,6 +65,7 @@ pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
     pub authorization_token: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub timeout_milliseconds: u64,
 }
 
